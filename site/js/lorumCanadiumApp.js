de238911c60c77cb1699befcebf50fa1f2a59b53
Wayne Simmerson <wsimmerson@gmail.com>
2015-06-08 21:26:44 -0400

    better sentance structure

--- site/js/lorumCanadiumApp.js
+++ site/js/lorumCanadiumApp.js
@@ -21,6 +21,7 @@ angular.module("lorumCanadiumApp", [])
                 $http.get("/api/canadianisms.json")
                     .success(function(data) {
                         $scope.data = data;
+                        console.log($scope.data);
                     })
                     .error(function(err) {
                         console.log(err);
@@ -38,21 +39,43 @@ angular.module("lorumCanadiumApp", [])
 
                 $scope.generateParagraph = function () {
 
-                    var paragraph = "";
-                    var sentancesNumber = Math.floor(Math.random() * (7 - 3) + 3);
+                    var capitalize = function(sen) {
+                        return sen.substring(0,1).toUpperCase() + sen.substring(1);
+                    };
 
-                    for (var n = 0; n < sentancesNumber; n++) {
+                    var paragraph = "";
+                    var sentancesNumber = Math.floor(Math.random() * (6 - 3) + 3);
+
+                    // for (var n = 0; n < sentancesNumber; n++) {
+                    //     var sentance = "";
+                    //     var sentanceLength = Math.floor(Math.random() * (15 - 7) + 7);
+                    //     for (var i = 0; i < sentanceLength; i++) {
+                    //         var word = Math.floor(Math.random() * ($scope.data.words.length -1));
+                    //         sentance += " " + $scope.data.words[word];
+                    //     }
+                    //     paragraph += capitalize(sentance);
+                    //     paragraph += $scope.punctuation[
+                    //         Math.floor(Math.random() * ($scope.punctuation.length -1))
+                    //     ];
+                    // }
+
+                    for (var n = 0; n < sentancesNumber; n++){
                         var sentance = "";
-                        var sentanceLength = Math.floor(Math.random() * (15 - 7) + 7);
-                        for (var i = 0; i < sentanceLength; i++) {
-                            var word = Math.floor(Math.random() * ($scope.data.words.length -1));
-                            sentance += " " + $scope.data.words[word];
-                        }
-                        paragraph += sentance;
-                        paragraph += $scope.punctuation[
-                            Math.floor(Math.random() * ($scope.punctuation.length -1))
+                        var template = $scope.data.templates[
+                            Math.floor(Math.random() * ($scope.data.templates.length -1) +1)
                         ];
+                        template = template.split(' ');
+
+                        for (var i = 0; i < template.length; i++) {
+                                sentance += " " + template[i].replace(
+                                    "temp",
+                                    $scope.data.words[Math.floor(Math.random() * ($scope.data.words.length -1) +1 )]
+                                );
+                        }
+                        sentance = sentance.replace("undefined", "");
+                        paragraph += " " + capitalize(sentance.trim());
                     }
+
                     return paragraph.trim();
                 };
 
